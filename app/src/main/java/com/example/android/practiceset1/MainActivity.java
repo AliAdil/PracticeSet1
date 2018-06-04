e48864d2d8957b42bb793fbbe1222b7c26a74b7a
aliadil <aliadilg@Gmail.com>
2018-06-05 03:05:23 +0500

    update

--- app/src/main/java/com/example/android/practiceset1/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset1/MainActivity.java
@@ -13,18 +13,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    int score = 0;
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
     public void one (View view){
-        displayForTeamA(1);
+        score += 1;
+        displayForTeamA(score);
     }
     public void two (View view){
-        displayForTeamA(2);
+        score += 2;
+        displayForTeamA(score);
     }
     public void  three(View view){
-        displayForTeamA(3);
+        score += 3;
+        displayForTeamA(score);
     }
 }
