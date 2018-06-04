ad7bac40a8013e3eb5b86987e8f7a02a809b006c
aliadil <aliadilg@Gmail.com>
2018-06-05 02:51:31 +0500

    update

--- app/src/main/java/com/example/android/practiceset1/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.practiceset1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,10 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+
     }
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    public void one (View view){
+        displayForTeamA(1);
+    }
+    public void two (View view){
+        displayForTeamA(2);
+    }
+    public void  three(View view){
+        displayForTeamA(3);
     }
 }
