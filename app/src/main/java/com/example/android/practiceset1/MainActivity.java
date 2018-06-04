bbdf1929ab02672eace8ea8afef44bc102fb01f2
aliadil <aliadilg@Gmail.com>
2018-06-05 03:20:25 +0500

    update

--- app/src/main/java/com/example/android/practiceset1/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset1/MainActivity.java
@@ -6,29 +6,29 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int scoreTeamA = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
-    int score = 0;
+
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
     public void one (View view){
-        score += 1;
-        displayForTeamA(score);
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
     public void two (View view){
-        score += 2;
-        displayForTeamA(score);
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
     public void  three(View view){
-        score += 3;
-        displayForTeamA(score);
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
     }
 }
